dfbabb71cab2729bbb3d81fbc91d55681e33f8e9
Kendrick Ronel Mundiz <98096938+kplus16@users.noreply.github.com>
2023-04-21 01:02:41 +0800

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
 export default function Hero(){
     return (
-        <section className="hero">
-            <h1>Hero Section</h1>
+        <section className="hero-container">
+            <div className="video-container">
+                <video autoPlay muted loop src="https://weareblazon.com/wp-content/uploads/2022/01/Blazon_HomepageLoop.mp4"></video>
+            </div>
+            <div className="header-container">
+                <img className="hero-heading" src="https://weareblazon.com/wp-content/uploads/2021/02/logo.svg"/>
+                <Link to="/quote" className="btn-heading"></Link>
+            </div>
         </section>
     )
 }
