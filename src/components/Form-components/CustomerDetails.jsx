520c91c40985fb08eaf2b6e22747b43f4a1ac538
Kendrick Ronel Mundiz <98096938+kplus16@users.noreply.github.com>
2023-04-27 04:51:24 +0800

    Form update

--- src/components/Form-components/CustomerDetails.jsx
+++ src/components/Form-components/CustomerDetails.jsx
@@ -2,11 +2,14 @@
 
 export default function CustomerDetails(){
     return (
+        <>
+        <h1>Contact Us!</h1>
         <div className="form-container mt-6">
             <input type="text" placeholder="First Name..." className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-100 focus:shadow-outline mb-3"></input>
             <input type="text" placeholder="Last Name..." className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-100 focus:shadow-outline mb-3"></input>
             <input type="email" placeholder="Email..." className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-100 focus:shadow-outline mb-3"></input>
             <input type="number" placeholder="Phone..." className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-100 focus:shadow-outline mb-3" min="1"></input>
         </div>
+        </>
     )
 }
